d40609455964b34f7dfdcaf90e87c1dcee0e2d6f
winfreyj <jbwinfrey@gmail.com>
2018-02-22 19:37:18 -0500

    Added function header and C files.

---
+++ functions.h
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef functions.h
+#define functions.h
+
+struct product{
+	char *name;
+	char *unit;
+	int price;
+	int quantity;
+};
+
+struct node{
+	struct product *data;
+	struct node *next;
+	struct node *prev;
+};
+
+// Creates an empty list - only a head node
+struct node *init();
+
+// Inserts a product into the list
+void insert(struct node *head, char *name, char *unit, int price,
+int quantity);
+
+// Deletes a product from the list
+void delete(struct node *head, char *name);
+
+// Deletes all products in the list
+void deleteAll(struct node *head);
+
+// Searches for a product in the list
+void search(struct node *head, char *name);
+
+// Displays all products in the list
+void display(struct node *head);
+
+// Adds one to the quantity of the given product
+void purchase(struct node *head, char *name);
+
+// Removes one from the quantity of the given product
+void sell(struct node *head, char *name);
+
+#endif
