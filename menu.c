735c6b27c1f1087f065f4525cff26ac9cdc2f6a5
Joseph <cutinoj@mail.gvsu.edu>
2018-03-02 21:47:21 -0500

    test

--- menu.c
+++ menu.c
@@ -98,6 +98,7 @@ int performAction(int command){
 			scanf("%s,%s,%d,%d",name,units,&price,&quantity);
 
 			//insert item in list
+			printf("Recieved: %s,%s,%d,%d\n",name,units,price,quantity);
 			insert(list,name,units,price,quantity);
 			break;
 		case 3:
